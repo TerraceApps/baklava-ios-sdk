b07df219dbdfb6848a900ca5e6dbdb78a6374336
Baklava CI <ci@terraceapps.com>
2026-05-08 07:30:19 +0000

    chore: release 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.3
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "BaklavaSDKBinary",
             url: "https://github.com/TerraceApps/baklava-ios-sdk/releases/download/v1.0.0/BaklavaSDK.xcframework.zip",
-            checksum: "fdbce5087ccf8662de7ca9d1bbe1436f69a83133aeff81876b0d9f42794e3d98"
+            checksum: "6272aa33fb8180d280aecb83dbc0d783f2d20accef2072d17889a2fd2e05461b"
         )
     ]
 )
