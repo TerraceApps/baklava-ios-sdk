43eb9a1a43261e55fd78d29a530a1bfc0573375d
Baklava CI <ci@terraceapps.com>
2026-05-11 15:36:29 +0000

    chore: release 1.0.0

--- Package.swift
+++ Package.swift
@@ -7,15 +7,10 @@ let package = Package(
         .library(name: "BaklavaSDK", targets: ["BaklavaSDK"])
     ],
     targets: [
-        .target(
-            name: "BaklavaSDK",
-            dependencies: [.target(name: "BaklavaSDKBinary")],
-            path: "Sources/BaklavaSDK"
-        ),
         .binaryTarget(
-            name: "BaklavaSDKBinary",
+            name: "BaklavaSDK",
             url: "https://github.com/TerraceApps/baklava-ios-sdk/releases/download/v1.0.0/BaklavaSDK.xcframework.zip",
-            checksum: "6272aa33fb8180d280aecb83dbc0d783f2d20accef2072d17889a2fd2e05461b"
+            checksum: "6d356e4c805dc275d1f9e17363840f4923278d44f92f8957fa676d4aff5beff6"
         )
     ]
 )
